4d7b96af3a1f9053d106ddfb6f32fc403094eecd
Mosè Giordano <mose@gnu.org>
2018-11-27 23:27:45 +0100

    Update the build script and add CMake-related files

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,49 +1,34 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-# These are the platforms built inside the wizard
-platforms = [
-    BinaryProvider.Linux(:i686, :glibc),
-    BinaryProvider.Linux(:x86_64, :glibc),
-    BinaryProvider.Linux(:aarch64, :glibc),
-    BinaryProvider.Linux(:armv7l, :glibc),
-    BinaryProvider.Linux(:powerpc64le, :glibc),
-    BinaryProvider.MacOS(),
-    BinaryProvider.Windows(:i686),
-    BinaryProvider.Windows(:x86_64)
-]
-
-
-# If the user passed in a platform (or a few, comma-separated) on the
-# command-line, use that instead of our default platforms
-if length(ARGS) > 0
-    platforms = platform_key.(split(ARGS[1], ","))
-end
-info("Building for $(join(triplet.(platforms), ", "))")
+name = "cspice"
+version = v"66"
 
 # Collection of sources required to build cspice
 sources = [
     "http://naif.jpl.nasa.gov/pub/naif/toolkit//C/PC_Linux_GCC_64bit/packages/cspice.tar.Z" =>
     "93cd4fbce5818f8b7fecf3914c5756b8d41fd5bdaaeac1f4037b5a5410bc4768",
-
-    "https://github.com/JuliaAstro/SPICE.jl.git" =>
-    "43504486e347f4c499ada07bd41299f58153b12b",
+    "./extra"
 ]
+
+# These are the platforms built inside the wizard
+platforms = supported_platforms()
 
 script = raw"""
 cd $WORKSPACE/srcdir
 cp -r cspice/src/cspice/ .
-cp SPICE.jl/deps/CMakeLists.txt .
-cp SPICE.jl/deps/src.cmake .
 cmake -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
 make
 make install
-
 """
 
 products = prefix -> [
-    LibraryProduct(prefix,"libcspice")
+    LibraryProduct(prefix, "libcspice", :libcspice)
 ]
 
-# Build the given platforms using the given sources
-hashes = autobuild(pwd(), "cspice", platforms, sources, script, products)
+# Dependencies that must be installed before this package can be built
+dependencies = []
 
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
